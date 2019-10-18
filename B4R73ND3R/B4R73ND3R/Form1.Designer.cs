ed631286fd570f61ecb373b6e682ed1735d1369e
Klemen2 <50655512+Klemen2@users.noreply.github.com>
2019-10-18 15:57:43 +0200

    Form1 blink blink

--- B4R73ND3R/B4R73ND3R/Form1.Designer.cs
+++ B4R73ND3R/B4R73ND3R/Form1.Designer.cs
@@ -28,21 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(83, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(287, 129);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 100;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 120;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 140;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 160;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(495, 129);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(716, 129);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(938, 407);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "/";
+            this.Load += new System.EventHandler(this.pictureBox1_Load);
+            this.Load += new System.EventHandler(this.pictureBox2_Load);
+            this.Load += new System.EventHandler(this.pictureBox3_Load);
+            this.Load += new System.EventHandler(this.pictureBox4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
     }
 }
 
--- B4R73ND3R/B4R73ND3R/Form1.cs
+++ B4R73ND3R/B4R73ND3R/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Gtec.Unicorn;
@@ -18,9 +19,55 @@ namespace B4R73ND3R
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
+
+        private void pictureBox1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Enabled = true;
+        }
+        private void pictureBox2_Load(object sender, EventArgs e)
+        {
+            timer2.Start();
+            timer2.Enabled = true;
+        }
+        private void pictureBox3_Load(object sender, EventArgs e)
+        {
+            timer3.Start();
+            timer3.Enabled = true;
+        }
+        private void pictureBox4_Load(object sender, EventArgs e)
+        {
+            timer4.Start();
+            timer4.Enabled = true;
+        }
+
+        Color red = Color.Red;
+        Color bg = Color.Transparent;
+        bool pic1 = false, pic2 = false, pic3 = false, pic4 = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pic1) pictureBox1.BackColor = red;
+            else pictureBox1.BackColor = bg;
+            pic1 = !pic1;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (pic2) pictureBox2.BackColor = red;
+            else pictureBox2.BackColor = bg;
+            pic2 = !pic2;
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (pic3) pictureBox3.BackColor = red;
+            else pictureBox3.BackColor = bg;
+            pic3 = !pic3;
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (pic4) pictureBox4.BackColor = red;
+            else pictureBox4.BackColor = bg;
+            pic4 = !pic4;
         }
     }
 }
